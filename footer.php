0c6c04aab7418681880f591b96c0296f847f1aac
Jonathan Zong <jonathan@jonathanzong.com>
2016-10-29 17:04:44 -0400

    hit counter

--- footer.php
+++ footer.php
@@ -8,5 +8,26 @@
       // ga('create','UA-XXXXX-X','auto');ga('send','pageview');
     </script>
     <?php wp_footer(); ?>
+
+    <div id="counter">
+      <div>
+        <?php 
+          $visits = vcp_get_visit_count('T');
+          $digits = str_split($visits);
+          $num_digits = strlen($visits);
+          for ($x = 0; $x < 9 - $num_digits; $x++):
+        ?>
+          <span class="number">0</span>
+        <?php
+          endfor;
+          for ($x = 0; $x < $num_digits; $x++):
+        ?>
+          <span class="number"><?php echo $digits[$x]; ?></span>
+        <?php endfor; ?>
+      </div>
+      <div class="tiny visitors">
+        Visitors
+      </div>
+    </div>
   </body>
 </html>
