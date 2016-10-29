556c093b3fcd471010f5f82c2eb21a2efb2ad862
Jonathan Zong <jonathan@jonathanzong.com>
2016-10-29 16:49:46 -0400

    hide menu items, add philosophy-inspiration content

--- page-philosophy-inspiration.php
+++ page-philosophy-inspiration.php
@@ -4,9 +4,23 @@
 <div class="content">
   <div id="page-<?php the_ID() ?>" <?php post_class() ?>>
     <h2 class="entry-title"><?php the_title() ?></h2>
-    <h4>find me in page-custom.php</h4>
     <div class="entry-content">
       <?php the_content() ?>
+      <?php the_field('philosophy_text') ?>
+      <?php 
+        $images = get_field('inspiration_gallery');
+
+        if( $images ): ?>
+            <ul>
+                <?php foreach( $images as $image ): ?>
+                    <li>
+                        <a href="<?php echo $image['url']; ?>">
+                             <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
     </div>
   </div><!-- .post -->
 </div><!-- .content -->
