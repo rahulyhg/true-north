8e316b215a8c3fb4c0a2c34f95edfd9cdaddc84e
Jonathan Zong <jonathan@jonathanzong.com>
2016-11-09 15:42:49 -0500

    mobile styles

--- partials/pins.php
+++ partials/pins.php
@@ -7,12 +7,29 @@
             $x = get_sub_field('pin_x');
             $y = get_sub_field('pin_y');
             $name = get_sub_field('name');
-            $minutes = get_sub_field('minutes_away');
+            $minutes_away = get_sub_field('minutes_away');
+            $hours = (int)($minutes_away / 60);
+            $minutes = $minutes_away % 60;
         ?>
             <div class="map-pin" style="top: <?php echo $y; ?>px;left: <?php echo $x; ?>px;">
               <div class="pin-svg"></div>
               <div class="pin-name"><?php echo $name; ?></div>
-              <div class="pin-minutes"><?php echo $minutes; ?> minutes</div>
+              <div class="pin-minutes">
+                <?php
+                  if ($hours > 1):
+                    echo $hours . " hours ";
+                  elseif ($hours == 1):
+                    echo $hours . " hour ";
+                  endif;
+                  if ($minutes > 1):
+                    echo $minutes . " minutes";
+                  elseif ($minutes == 1):
+                    echo $minutes . " minute";
+                  elseif ($minutes == 0 && $hours == 0):
+                    echo $minutes . " minutes";
+                  endif;
+                ?>
+              </div>
             </div>
         <?php
           endwhile;
