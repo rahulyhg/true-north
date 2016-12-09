bc811fec95de0aca74b19a4975c6c4e9a16fe00a
Jonathan Zong <jonathan@jonathanzong.com>
2016-12-08 20:08:27 -0500

    check for sent

--- page-contact.php
+++ page-contact.php
@@ -7,21 +7,29 @@
   </div><!-- .post -->
 </div><!-- .content -->
 <div class="contact-form">
-  <form action="/webformmailer.php" method="post">
-    <input type="hidden" name="subject" value="True North Contact Form" />
-    <input type="hidden" name="redirect" value="/contact?sent=true" />
-    <label>Name <input type="text" name="Name" minlength="2" required/> </label>
-    <label>Email <input type="email" name="Email" required/> </label>
-    <label>Current City <input type="text" name="CurrentCity" required/> </label>
-    <label>Desired Unit(s) <input type="text" name="DesiredUnits" required/> </label>
-    <label>Desired Move-in <input type="text" name="DesiredMovein" required/> </label>
-    <label>Desired Lease Term <input type="text" name="DesiredLeaseTerm" required/> </label>
-    <label>Additional Feedback <textarea name="AdditionalFeedback"></textarea> </label>
-    <input type="submit" name="submit" value="submit"/>
-    <input type="hidden" name="form_order" value="default"/>
-    <input type="hidden" name="form_delivery" value="default"/>
-    <input type="hidden" name="form_format" value="html"/>
-  </form>
+  <?php
+    if (isset($_GET["sent"])):
+  ?>
+  <div>Your message has been sent.</div>
+  <?php
+    else:
+  ?>
+    <form action="/webformmailer.php" method="post">
+      <input type="hidden" name="subject" value="True North Contact Form" />
+      <input type="hidden" name="redirect" value="/contact?sent" />
+      <label>Name <input type="text" name="Name" minlength="2" required/> </label>
+      <label>Email <input type="email" name="Email" required/> </label>
+      <label>Current City <input type="text" name="CurrentCity" required/> </label>
+      <label>Desired Unit(s) <input type="text" name="DesiredUnits" required/> </label>
+      <label>Desired Move-in <input type="text" name="DesiredMovein" required/> </label>
+      <label>Desired Lease Term <input type="text" name="DesiredLeaseTerm" required/> </label>
+      <label>Additional Feedback <textarea name="AdditionalFeedback"></textarea> </label>
+      <input type="submit" name="submit" value="submit"/>
+      <input type="hidden" name="form_order" value="default"/>
+      <input type="hidden" name="form_delivery" value="default"/>
+      <input type="hidden" name="form_format" value="html"/>
+    </form>
+  <?php endif; ?>
 </div>
 
 <script>
